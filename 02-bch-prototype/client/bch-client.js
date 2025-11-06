a375b30f7d18a8c4e345b7f288b2fe252f45402f
Chris Troutner <chris.troutner@gmail.com>
2025-11-06 08:10:55 -0800

    Added BCH middleware for handling X-PAYMENT header

---
+++ 02-bch-prototype/client/bch-client.js
@@ -0,0 +1,67 @@
+/*
+  Demo x402 API client.
+*/
+
+// Global npm libraries
+import { config } from 'dotenv'
+import axios from 'axios'
+
+async function main () {
+  try {
+    // Load environment variables
+    config()
+
+    const privateKey = process.env.PRIVATE_KEY || '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef'
+    const baseURL = process.env.RESOURCE_SERVER_URL || 'http://localhost:4021'
+    const endpointPath = process.env.ENDPOINT_PATH || '/weather'
+
+    if (!baseURL || !privateKey || !endpointPath) {
+      console.error('Missing required environment variables')
+      process.exit(1)
+    }
+
+    // Step 1: Make a normal API call and expect a 402 error
+    console.log('\n\nStep 1: Making first call, expecting a 402 error returned.')
+    try {
+      const response = await axios.get(baseURL + endpointPath)
+      console.log(response.data)
+      console.log('Step 1 failed. Expected a 402 error.')
+    } catch (err) {
+      console.log(`Status code: ${err.response.status}`)
+      console.log(`Error data: ${JSON.stringify(err.response.data, null, 2)}`)
+      console.log('\n\n')
+    }
+
+    // Step 2: Make a second call with a payment.
+    // console.log('\n\nStep 2: Making second call with a payment.')
+
+    // try {
+    //   // Create a signer from the private key.
+    //   const signer = await createSigner('base-sepolia', privateKey)
+
+    //   // Wrap axios with the payment interceptor for automatic payment and
+    //   // retry when the 402 error is encountered.
+    //   const api = withPaymentInterceptor(
+    //     axios.create({
+    //       baseURL
+    //     }),
+    //     signer
+    //   )
+
+    //   // Call the same endpoint path with a payment.
+    //   const response = await api.get(endpointPath)
+    //   console.log('Data returned after payment: ', response.data)
+    // } catch (err) {
+    //   console.log('Step 2 failed. Expected a 200 success status code.')
+    //   console.log(`Status code: ${err.response.status}`)
+    //   console.log(`Error data: ${JSON.stringify(err.response.data, null, 2)}`)
+
+    //   // Decode the payment response from the header.
+    //   const paymentResponse = decodeXPaymentResponse(err.response.config.headers['X-PAYMENT'])
+    //   console.log(paymentResponse)
+    // }
+  } catch (err) {
+    console.error('Error starting client:', err)
+  }
+}
+main()
